ffeefb9e1864eeeff38b21e6cf687ea91fd68df6
turvec <okaforturvec@gmail.com>
2022-02-07 23:37:21 +0100

    initial

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,18 @@
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+const Token = artifacts.require("Token");
+
+module.exports = async function(deployer) {
+  //Deploy turv token
+  await deployer.deploy(Token);
+
+  const token = await Token.deployed()
+
+  //Deploy ethswap
+  await deployer.deploy(EthSwap);
+
+  const ethSwap = await EthSwap.deployed()
+
+  //Tranfer all turv token to ethSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
